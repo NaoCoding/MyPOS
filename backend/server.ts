6b88c4ae6acf3269660d9037d45055e196cf1d98
mrfish233 <baboom003@gmail.com>
2025-05-09 14:42:09 +0800

    style: change code sequence of server.ts
    Adding semicolons to each line and change the import order

--- backend/server.ts
+++ backend/server.ts
@@ -1,24 +1,25 @@
 // load environment variables before importing other modules
-import dotenvFlow from 'dotenv-flow'
+import dotenvFlow from 'dotenv-flow';
 dotenvFlow.config();
 
-import express from 'express'
-import cors from 'cors'
-import loginRouter from './routes/login'
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+
+import loginRouter from './routes/login';
 
 const BACKEND_PORT = process.env.BACKEND_PORT || 5000;
 const corsOpt = {
     origin: "http://localhost:3000",
     credentials: true,
-}
+};
 
-const app = express()
-app.use(cors(corsOpt))
-app.use(cookieParser())
-app.use(express.json())
+const app = express();
+app.use(cors(corsOpt));
+app.use(cookieParser());
+app.use(express.json());
 
-app.use(loginRouter)
+app.use(loginRouter);
 
 app.listen(BACKEND_PORT, () => {
     console.log(`Server is running on port ${BACKEND_PORT}`);
