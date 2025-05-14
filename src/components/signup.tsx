f94b50534e937f907fd25a3ad5340ba8bf55a5b0
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2025-05-14 13:30:04 +0800

    feat : nextjs-toploader + SignUp component

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 //TODO : connect with backend and backend should return a token
 interface Credentials {
     username: string;
+    telphone: string;
+    email: string;
     password: string;
 }
 
@@ -14,7 +16,10 @@ interface LoginProps {
 
 export default function Login() {
     const [username, setUserName] = useState<string>('');
+    const [telphone, setTelphone] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         //ToDo : fetch backend api and get token
@@ -25,22 +30,31 @@ export default function Login() {
             <img className="image-container" src="./images/shop_img.jpg"></img>
             <form className="login-box" onSubmit={handleSubmit}>
                 <div className="login-header">
-                    <header>Login</header>
+                    <header>Sign Up</header>
                 </div>
                 <div className="input-box">
-                    <input type="text" className="input-field" placeholder="Email / Username" autoComplete="on" required onChange={e => setUserName(e.target.value)} />
+                    <input type="text" className="input-field" placeholder="Username" autoComplete="off" required onChange={e => setUserName(e.target.value)} />
+                </div>
+                <div className="input-box">
+                    <input type="text" className="input-field" placeholder="Email" autoComplete="on" required onChange={e => setEmail(e.target.value)} />
+                </div>
+                <div className="input-box">
+                    <input type="text" className="input-field" placeholder="Telphone" autoComplete="off" required onChange={e => setTelphone(e.target.value)} />
                 </div>
                 <div className="input-box">
                     <input type="password" className="input-field" placeholder="Password" autoComplete="off" required onChange={e => setPassword(e.target.value)} />
                 </div>
+                <div className="input-box">
+                    <input type="password" className="input-field" placeholder="Confirm Password" autoComplete="off" required onChange={e => setConfirmPassword(e.target.value)} />
+                </div>
                 
                 <div className="input-submit">
                     <button type="submit" className="submit-btn" id="submit">
-                        <label htmlFor="submit">Sign In</label>
+                        <label htmlFor="submit">Sign Up</label>
                     </button>
                 </div>
                 <div className="sign-up-label">
-                    <label htmlFor="sign-up">Don't have an account? <a href="/signup">Sign Up</a></label>
+                    <label htmlFor="sign-up">Already Have an account? <a href="/login">Login</a></label>
                 </div>
             </form>
         </>
