6cde2d62b7a76b5f9597a331fead4600aa40dd8c
mrfish233 <baboom003@gmail.com>
2025-05-19 00:56:01 +0800

    feat: add middleware to check login

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { findUserSession } from './models/user';
 
 export function checkNotLogin(req: Request, res: Response, next: NextFunction) {
     const token = req.cookies.token;
@@ -6,6 +8,46 @@ export function checkNotLogin(req: Request, res: Response, next: NextFunction) {
     if (token) {
         res.status(400).json({
             message: "已登入",
+        });
+        return;
+    }
+
+    next();
+}
+
+export async function checkLogin(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies.token;
+
+    if (!token) {
+        res.status(400).json({
+            message: "未登入",
+        });
+        return;
+    }
+
+    try {
+        const JWT_TOKEN = process.env.JWT_TOKEN;
+        if (!JWT_TOKEN) {
+            throw new Error("JWT_TOKEN is not defined in .env file");
+        }
+
+        const decoded = jwt.verify(token, JWT_TOKEN);
+
+        if (!decoded || typeof decoded === 'string' || !('username' in decoded)) {
+            throw new Error("Invalid token payload");
+        }
+
+        const userSession = await findUserSession({ username: decoded.username });
+
+        if (!userSession || userSession.token !== token) {
+            res.clearCookie('token');
+            throw new Error("User session not found or token mismatch");
+        }
+    }
+    catch (error) {
+        console.error("JWT verification error:", error);
+        res.status(401).json({
+            message: "token 驗證失敗",
         });
         return;
     }
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -36,3 +36,27 @@ export async function findUser(data: Partial<User>) {
 
     return await query.selectAll().executeTakeFirst();
 }
+
+export async function findUserSession(data: Partial<User>) {
+    let query = db.selectFrom('user').innerJoin('session', 'user.id', 'session.user_id');
+
+    if (data.id) {
+        query = query.where('user.id', '=', data.id);
+    }
+
+    if (data.username) {
+        query = query.where('user.username', '=', data.username);
+    }
+
+    if (data.email) {
+        query = query.where('user.email', '=', data.email);
+    }
+
+    if (data.telephone) {
+        query = query.where('user.telephone', '=', data.telephone);
+    }
+
+    return await query.select([
+        'user.id', 'user.username', 'session.token', 'session.expired_at',
+    ]).executeTakeFirst();
+}
