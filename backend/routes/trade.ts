c6d78ebef5e8e405ef85bfbcad506e82982037de
mrfish233 <baboom003@gmail.com>
2025-06-09 02:44:31 +0800

    feat: insert item customizations into table when creating a trade

--- backend/routes/trade.ts
+++ backend/routes/trade.ts
@@ -4,49 +4,43 @@ import { checkLogin } from '../middleware';
 import { findUser } from '../models/user';
 import { createTradeItem, deleteTradeItem, updateTradeItem } from '../models/trade_item';
 import { findItem, findItemsByTradeID } from '../models/item';
+import { findCustomizationGroupByItemID } from '../models/customization_group';
+import { findCustomization, findCustomizationsByGroupID } from '../models/customization';
+import { createTradeItemCustomization } from '../models/trade_item_customization';
 
 const tradeRouter = Router();
 tradeRouter.use(checkLogin);
 
 tradeRouter.post('/', async (req: Request, res: Response) => {
-    const { user_id, items } = req.body;
+    const { user_id, trade_items } = req.body;
 
     // Validate request body
-    if (!user_id || !items) {
+    if (!user_id || !trade_items) {
         res.status(400).json({
             message: "User ID and status are required"
         });
         return;
     }
-    else if (!Array.isArray(items) || items.length === 0) {
+    else if (!Array.isArray(trade_items) || trade_items.length === 0) {
         res.status(400).json({
             message: "Items must be a non-empty array"
         });
         return;
     }
 
-    let itemIds: number[] = [];
-    for (const item of items) {
-        if (!item.id || !item.quantity) {
+    for (const trade_item of trade_items) {
+        if (!trade_item.id || !trade_item.quantity) {
             res.status(400).json({
                 message: "Each item must have an ID and quantity"
             });
             return;
         }
-        if (itemIds.includes(item.id)) {
-            res.status(400).json({
-                message: "Duplicate item IDs are not allowed"
-            });
-            return;
-        }
-        if (item.quantity <= 0) {
+        if (trade_item.quantity <= 0) {
             res.status(400).json({
                 message: "Item quantity must be greater than 0"
             });
             return;
         }
-
-        itemIds.push(item.id);
     }
 
     try {
@@ -57,13 +51,8 @@ tradeRouter.post('/', async (req: Request, res: Response) => {
             return;
         }
 
-        const trade = await createTrade({
-            user_id,
-            status: 'pending',
-        });
-
-        for (const data of items) {
-            const item = await findItem({ id: data.id });
+        for (const trade_item of trade_items) {
+            const item = await findItem({ id: trade_item.id });
             if (!item) {
                 res.status(400).json({
                     message: "Item with this ID does not exist"
@@ -71,11 +60,42 @@ tradeRouter.post('/', async (req: Request, res: Response) => {
                 return;
             }
 
-            await createTradeItem({
+            await checkCustomizations(trade_item.id, trade_item.customizations || []);
+        }
+
+        const trade = await createTrade({
+            user_id,
+            status: 'pending'
+        });
+
+        for (const trade_item of trade_items) {
+            const newTradeItem = await createTradeItem({
                 trade_id: Number(trade.insertId),
-                item_id: data.id,
-                quantity: data.quantity
+                item_id: trade_item.id,
+                quantity: trade_item.quantity
             });
+            const tradeItemId = Number(newTradeItem.insertId);
+
+            if (!trade_item.customizations || trade_item.customizations.length === 0) {
+                continue;
+            }
+
+            for (const customizationId of trade_item.customizations) {
+                const customization = await findCustomization({ id: customizationId });
+
+                if (!customization) {
+                    res.status(400).json({
+                        message: `Customization with ID ${customizationId} does not exist`
+                    });
+                    return;
+                }
+
+                await createTradeItemCustomization({
+                    trade_item_id: tradeItemId,
+                    customization_id: customization.id,
+                    price_delta_snapshot: customization.price_delta
+                });
+            }
         }
 
         res.status(201).json({
@@ -83,7 +103,8 @@ tradeRouter.post('/', async (req: Request, res: Response) => {
         });
     } catch (error) {
         res.status(500).json({
-            message: "Error creating trade"
+            message: "Error creating trade",
+            error
         });
     }
 });
@@ -234,5 +255,61 @@ tradeRouter.delete('/:id', async (req: Request, res: Response) => {
         });
     }
 });
+
+async function checkCustomizations(itemId: number, itemCustomizationIds: any[]): Promise<boolean> {
+    if (!Array.isArray(itemCustomizationIds) || itemCustomizationIds.length === 0) {
+        return true;
+    }
+
+    const customizationGroups = await findCustomizationGroupByItemID(itemId);
+    if (!customizationGroups || customizationGroups.length === 0) {
+        if (itemCustomizationIds.length > 0) {
+            console.error("Item does not support customizations");
+            throw new Error("Item does not support customizations");
+        }
+
+        return true;
+    }
+
+    for (const customizationId of itemCustomizationIds) {
+        const customization = await findCustomization({ id: customizationId });
+        if (!customization) {
+            console.error(`Customization with ID ${customizationId} does not exist.`);
+            throw new Error(`Customization with ID ${customizationId} does not exist.`);
+        }
+    }
+
+    let validCustomizationCount = 0;
+
+    for (const customizationGroup of customizationGroups) {
+        if (!customizationGroup.customization_group_id) {
+            continue;
+        }
+
+        const customizations = await findCustomizationsByGroupID(customizationGroup.customization_group_id);
+        const customizationIds = customizations.map(c => c.id);
+
+        console.log(`Checking customizations for group ${customizationGroup.name}:`, customizationIds);
+
+        let min = customizationGroup.is_required ? 1 : 0;
+        let max = customizationGroup.is_multiple_choice ? customizations.length : 1;
+
+        const selectedCustomizations = itemCustomizationIds.filter(id => customizationIds.includes(id));
+
+        if (selectedCustomizations.length < min || selectedCustomizations.length > max) {
+            console.error(`Invalid number of customizations for group ${customizationGroup.name}. Expected between ${min} and ${max}, but got ${selectedCustomizations.length}.`);
+            throw new Error(`Invalid number of customizations for group ${customizationGroup.name}. Expected between ${min} and ${max}, but got ${selectedCustomizations.length}.`);
+        }
+
+        validCustomizationCount += selectedCustomizations.length;
+    }
+
+    if (validCustomizationCount !== itemCustomizationIds.length) {
+        console.error("Some customizations are not valid for the selected item.");
+        throw new Error("Some customizations are not valid for the selected item.");
+    }
+
+    return true;
+}
 
 export default tradeRouter;
