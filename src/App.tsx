197246cd32e99e71d530c4465313248f59060b00
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2025-06-09 00:27:41 +0800

    feat: connect user order page with backend

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ import NoteAnalysis from './pages/NoteAnalysis';
 import CustomerAnalysis from './pages/CustomerAnalysis';
 import SalesRanking from './pages/SalesRanking';
 import Inventory from './pages/Inventory';
-import User_Home from './pages/user/Home';
-import User_Order from './pages/user/Order';
-import User_OrderSubmit from './pages/user/OrderSubmit';
-import User_OrderHistory from './pages/user/OrderHistory';
-import User_CustomerInfo from './pages/user/CustomerInfo';
+import UserHome from './pages/user/Home';
+import UserOrder from './pages/user/Order';
+import UserOrderSubmit from './pages/user/OrderSubmit';
+import UserOrderHistory from './pages/user/OrderHistory';
+import UserCustomerInfo from './pages/user/CustomerInfo';
 
 function App() {
   const [token, setToken] = useState<string>('');
@@ -90,11 +90,12 @@ function App() {
         <NextTopLoader color='#565656' showSpinner={false} />
         <Navbar role_id={roleId} setToken={setToken} />
         <Routes>
-          <Route path="/user/Home" element={<User_Home />} />
-          <Route path="/user/Order" element={<User_Order />} />
-          <Route path="/user/Submit" element={<User_OrderSubmit />} />
-          <Route path="/user/History" element={<User_OrderHistory />} />
-          <Route path="/user/CustomerInfo" element={<User_CustomerInfo />} />
+          <Route path="/" element={<UserHome />} />
+          <Route path="/user/Home" element={<UserHome />} />
+          <Route path="/user/Order" element={<UserOrder />} />
+          <Route path="/user/Submit" element={<UserOrderSubmit />} />
+          <Route path="/user/History" element={<UserOrderHistory />} />
+          <Route path="/user/CustomerInfo" element={<UserCustomerInfo />} />
         </Routes>
       </div>
     );
@@ -136,11 +137,11 @@ function App() {
             <Route path="/manage/user" element={<UserManagement/>} />
           }
 
-          <Route path="/user/Home" element={<User_Home />} />
-          <Route path="/user/Order" element={<User_Order />} />
-          <Route path="/user/Submit" element={<User_OrderSubmit />} />
-          <Route path="/user/History" element={<User_OrderHistory />} />
-          <Route path="/user/CustomerInfo" element={<User_CustomerInfo />} />
+          <Route path="/user/Home" element={<UserHome />} />
+          <Route path="/user/Order" element={<UserOrder />} />
+          <Route path="/user/Submit" element={<UserOrderSubmit />} />
+          <Route path="/user/History" element={<UserOrderHistory />} />
+          <Route path="/user/CustomerInfo" element={<UserCustomerInfo />} />
 
         </Routes>
 
