5d1b3322af4dba51ddbd10cccf67b729ed8f2c9b
mrfish233 <baboom003@gmail.com>
2025-06-08 19:27:51 +0800

    feat: throw error during migration failure
    Instead of logging the error and returning during database migrations, this change now throws the error.

--- backend/init.ts
+++ backend/init.ts
@@ -8,23 +8,29 @@ async function init() {
     dotenvFlow.config();
     console.log("Environment variables loaded");
 
-    // migrate down the last migration (DANGEROUS, use with caution)
-    // await migrateDown();
-    // console.log("Database migrations rolled back");
-    // await migrateDown();
-    // migrate down all migrations (DANGEROUS, use with caution)
-    // await migrateDownAll();
-    // console.log("All database migrations rolled back");
-
     cron.schedule('0 8 * * *', async () => {
         console.log("Deleting expired sessions");
         await deleteExpiredSessions();
         console.log("Expired sessions deleted");
     });
 
-    // run database migrations
-    await migrateToLatest();
-    console.log("Database migrations completed");
+    try {
+        // migrate down the last migration (DANGEROUS, use with caution)
+        // await migrateDown();
+        // console.log("Database migrations rolled back");
+
+        // migrate down all migrations (DANGEROUS, use with caution)
+        // await migrateDownAll();
+        // console.log("All database migrations rolled back");
+
+        // run database migrations
+        await migrateToLatest();
+        console.log("Database migrations completed");
+    }
+    catch (error) {
+        console.error("Error during database migration:", error);
+        process.exit(1);
+    }
 }
 
 init();
